053078c92fd6cfe865746271c6ba3f65e24d0455
TommasoAmato <tommaso.amato1993@gmail.com>
2024-12-30 18:31:33 +0000

    CREAZIONE NUOVE TABELLE

---
+++ TryDbProjects/Dipendente.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[Dipendente]
+(
+    [Id_Dipendente] INT NOT NULL PRIMARY KEY,
+    [Nome] VARCHAR (50) NOT NULL,
+    [Cognome] VARCHAR (50) NOT NULL,
+    [Data_Nascita] DATE NOT NULL
+)
