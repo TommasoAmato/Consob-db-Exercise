96259fefc3c52556dd8f18cda358f8d3eeaa54bb
TommasoAmato <tommaso.amato1993@gmail.com>
2024-12-30 18:47:54 +0000

    test vari

---
+++ Consob/Dipendente.sql
@@ -0,0 +1,9 @@
+CREATE TABLE [dbo].[Dipendente]
+(
+    [Id_Dipendente] INT NOT NULL PRIMARY KEY,
+    [Nome] VARCHAR (50) NOT NULL,
+    [Cognome] VARCHAR (50) NOT NULL,
+    [Data_Nascita] DATE NOT NULL
+)
+
+-- Write your own SQL object definition here, and it'll be included in your package.
