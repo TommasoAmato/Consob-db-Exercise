002729cc3910cca2a1d958f350a9cb4364d05f0c
TommasoAmato <tommaso.amato1993@gmail.com>
2025-01-02 11:51:42 +0000

    query finite

--- Consob/Parte C: Query SQL.sql
+++ Consob/Parte C: Query SQL.sql
@@ -21,9 +21,27 @@
 
 --Trova i progetti che coinvolgono almeno un dipendente con stipendio superiore a 70.000.
 
-SELECT p.id_progetto, P.Descrizione FROM Progetti P
-inner join AssegnazioneProgetti AP on AP.progetto = P.id_progetto
-inner join Dipendente D on D.Id_Dipendente = AP.dipendente
-inner join Ruolo R on R.Id_Ruolo = D.Ruolo
-WHERE R.Stipendio_Base >= 50000
-GROUP BY p.id_progetto
+--SELECT p.id_progetto, CAST(P.Descrizione AS nvarchar(MAX)) FROM Progetti P
+--inner join AssegnazioneProgetti AP on AP.progetto = P.id_progetto
+--inner join Dipendente D on D.Id_Dipendente = AP.dipendente
+--inner join Ruolo R on R.Id_Ruolo = D.Ruolo
+--WHERE R.Stipendio_Base >= 50000 AND d.Id_Dipendente IS NOT NULL
+--GROUP BY p.id_progetto, CAST(P.Descrizione AS nvarchar(MAX)), D.Nome
+
+
+--Elenca i dipendenti che non lavorano su alcun progetto.
+--SELECT D.NOME, D.COGNOME FROM Dipendente D
+--WHERE D.Id_Dipendente NOT IN (SELECT AP.Dipendente FROM AssegnazioneProgetti AP)
+--ORDER BY D.Nome
+
+--Per ogni dipendente, calcola il numero totale di ore settimanali assegnate e verifica se ci sono violazioni del vincolo massimo (40 ore).
+
+SELECT D.NOME, D.COGNOME, SUM (AP.ore) AS TOTALE_ORE,
+CASE
+    WHEN SUM(AP.Ore)>40 THEN 'VIOLAZIONE'
+    ELSE 'CONFORME'
+END AS STATO
+FROM Dipendente D
+INNER JOIN AssegnazioneProgetti AP ON D.Id_Dipendente = AP.dipendente
+GROUP BY D.Nome, D.Cognome
+ORDER BY TOTALE_ORE DESC
